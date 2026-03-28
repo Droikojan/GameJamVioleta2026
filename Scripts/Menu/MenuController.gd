7a5f88cee004ae864e4b6b1ed295f38e8b8994ab
Droir <tomasx1994@gmail.com>
2026-03-28 09:26:10 +0000

    Button Play implemented

--- Scripts/Menu/MenuController.gd
+++ Scripts/Menu/MenuController.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().get_first_node_in_group("TransitionController").load_level("uid://cow3mdict3hp4")
 
 func _on_settings_button_pressed() -> void:
 	MenuSettings.show()
