6bdf960615c10203dbf77d44bb3c84d1d5e8add5
Droir <tomasx1994@gmail.com>
2026-03-28 10:34:48 +0000

    Settings in all game

--- Scripts/Menu/MenuController.gd
+++ Scripts/Menu/MenuController.gd
@@ -4,7 +4,7 @@ func _on_play_button_pressed() -> void:
 	get_tree().get_first_node_in_group("TransitionController").load_level("uid://cow3mdict3hp4")
 
 func _on_settings_button_pressed() -> void:
-	MenuSettings.show()
+	MenuSettings.show_settings()
 
 func _on_creddits_button_pressed() -> void:
 	get_tree().get_first_node_in_group("TransitionController").load_level("uid://ydaiibkxuya0")
--- Scripts/Menu/Menu_settings.gd
+++ Scripts/Menu/Menu_settings.gd
@@ -3,11 +3,15 @@ extends CanvasLayer
 @export var music_volume_slider: HSlider
 @export var sfx_volume_slider: HSlider
 
+@export var settings_button: Control
+@export var settings: Control
+
 var music_bus : int
 var sfx_bus : int
 
 func _ready() -> void:
-	hide()
+	settings.hide()
+	settings.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	
 	music_bus = AudioServer.get_bus_index("Music")
 	sfx_bus = AudioServer.get_bus_index("SFX")
@@ -15,8 +19,20 @@ func _ready() -> void:
 	music_volume_slider.value = ConfigSave.load_music_volume_config()
 	sfx_volume_slider.value = ConfigSave.load_effects_volume_config()
 
+func _process(_delta: float) -> void:
+	if get_tree().get_nodes_in_group("StartMenu").size() > 0 or settings.visible:
+		settings_button.hide()
+	else:
+		settings_button.show()
+
+func show_settings():
+	settings.show()
+
+func _on_settings_button_pressed() -> void:
+	settings.show()
+
 func _on_back_button_pressed() -> void:
-	hide()
+	settings.hide()
 	ConfigSave.save_config(music_volume_slider.value, sfx_volume_slider.value)
 
 func _on_music_volume_value_changed(value: float) -> void:
